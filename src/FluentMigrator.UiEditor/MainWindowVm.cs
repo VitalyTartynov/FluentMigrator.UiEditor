fd7d45a56651bee3155ac29b43cd15e1061a674d
Vitaly.Tartynov <Vitaly.Tartynov@GMail.com>
2017-10-03 22:29:50 +0700

    Add simple main menu and vm for main window

---
+++ src/FluentMigrator.UiEditor/MainWindowVm.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
+namespace FluentMigrator.UiEditor
+{
+    public class MainWindowVm
+    {
+        #region -- OpenCommand --
+        public ICommand OpenCommand => new RelayCommand(OpenCommandExecute, () => false);
+
+        private void OpenCommandExecute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion -- OpenCommand --
+
+        #region -- SaveCommand --
+        public ICommand SaveCommand => new RelayCommand(SaveCommandExecute, () => false);
+
+        private void SaveCommandExecute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion -- SaveCommand --
+
+        #region -- QuitCommand --
+        public ICommand QuitCommand => new RelayCommand(QuitCommandExecute, () => true);
+
+        private void QuitCommandExecute()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+        #endregion -- QuitCommand --
+    }
+}
