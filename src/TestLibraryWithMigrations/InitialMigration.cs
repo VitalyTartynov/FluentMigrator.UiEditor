294d44aad6bfc1535bc4fd50f5133bfa342632f2
TartynovViA <TartynovViA@tomsknipi.ru>
2017-10-03 18:22:13 +0700

    Added test library for test decompilation

---
+++ src/TestLibraryWithMigrations/InitialMigration.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace TestLibraryWithMigrations
+{
+    [Migration(1, "Some description")]
+    public class InitialMigration : Migration
+    {
+        public override void Up()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
